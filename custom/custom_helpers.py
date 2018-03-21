2b248ecc26869fa502055d4a3c3fe37dac17fe75
Solara570 <solara570@163.com>
2018-03-21 13:41:46 +0800

    Fixed a typo that nobody cares about

--- custom/custom_helpers.py
+++ custom/custom_helpers.py
@@ -26,7 +26,7 @@ def mobs_shuffle(mobs):
     return mobs
 
 def tweak_color(color1, color2, alpha = 0.3):
-    """Return a weight average of two colors."""
+    """Return a weighted average of two colors."""
     alpha = clamp(0, 1, alpha)
     tweaked_rgb = alpha * color_to_rgb(color2) + (1-alpha) * color_to_rgb(color1)
     return rgb_to_color(tweaked_rgb)
